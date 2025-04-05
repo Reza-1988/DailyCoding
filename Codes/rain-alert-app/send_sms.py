95192470c9feee1f1fb62ab6738881399e7aad14
Reza-1988 <moha.rezahosseini@gmail.com>
2025-04-05 18:25:39 +0200

    Deleted the token and account sid

--- Codes/rain-alert-app/send_sms.py
+++ Codes/rain-alert-app/send_sms.py
@@ -4,13 +4,13 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "ACbf81ee23128bb9a3573aef31446d94a7"
-auth_token = "8b515bd3096c082b2122a7cbf39ed7dd"
+account_sid = "<Your Twilio Account SID>"
+auth_token = "<Your Auth Token>"
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body="It's going to rain today. remember to bring an umbrella.☂️",
-    from_="+15343447977",
+    from_="<Your Twilio Number>",
     to="<YOUR VERIFICATION NUMBER>",
 )
 
