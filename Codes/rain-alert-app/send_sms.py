ba08969e4eeb1219129b97d805f1ecbcbaea5a39
Reza-1988 <moha.rezahosseini@gmail.com>
2025-04-05 18:49:10 +0200

    Enhanced security by storing API key and token in environment variables

--- Codes/rain-alert-app/send_sms.py
+++ Codes/rain-alert-app/send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "<Your Twilio Account SID>"
-auth_token = "<Your Auth Token>"
+account_sid = os.environ.get("api_key")
+auth_token = os.environ.get("auth_token")
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
