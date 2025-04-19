71ef318feff4931c861022856af007ee54417a67
Reza-1988 <moha.rezahosseini@gmail.com>
2025-04-19 20:35:31 +0200

    Updated sample_settings

--- Codes/sample_settings.py
+++ Codes/sample_settings.py
@@ -3,10 +3,20 @@ FLIGHT_DATA = {
     "AMADEUS_API_SECRET": ""
 }
 
+SHEETY_DATA = {
+    "SHEETY_ENDPOINT_PRICES" : "",
+    "SHEETY_ENDPOINT_USERS" : ""
+}
 
 TWILIO_DATA = {
     "ACCOUNT_SID": "",
     "AUTH_TOKEN": "",
     "TWILIO_PHONE_NUMBER": "",
     "TO_PHONE_NUMBER": "",
+}
+
+SMTP_DATA = {
+    "EMAIL": "",
+    "PASSWORD": "",
+    "SMTP_SERVER": "",
 }
