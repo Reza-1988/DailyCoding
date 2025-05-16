9465f818fbcfecc8b2039d87578736a77c8db976
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-05-16 17:52:10 +0200

    Started webscraping bs4 100 movies with using beautifulsoup and requests module

---
+++ Codes/45- webscraping-bs4-100-movies/solutiion/solotion_100_movies_must_watch.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+
+soup = BeautifulSoup(response.text, "lxml")
+print(soup.prettify())
