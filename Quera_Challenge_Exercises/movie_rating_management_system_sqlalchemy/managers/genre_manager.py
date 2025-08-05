b30383793b58c6ca3362a6da70c18e98f11acd9b
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-08-05 19:27:29 +0200

    add new functions in genre manager in movie rating project

--- Quera_Challenge_Exercises/movie_rating_management_system_sqlalchemy/managers/genre_manager.py
+++ Quera_Challenge_Exercises/movie_rating_management_system_sqlalchemy/managers/genre_manager.py
@@ -1,8 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 
 
-from .. models import Genre
+from .. models import Genre, MovieGenre
 
 
 class GenreManager:
@@ -28,3 +28,31 @@ class GenreManager:
     def get_genre_by_name(self, name: str) -> Genre | None:
         stmt = select(Genre).where(Genre.name == name)
         return self.session.execute(stmt).scalar_one_or_none()
+
+    def update(self, genre_id: int, new_name: str) -> Genre | None:
+        genre = self.session.get(Genre, genre_id)
+        genre.name = new_name
+        self.session.commit()
+        return genre if genre else None
+
+    def delete(self, genre_id: int) -> bool:
+        genre = self.session.get(Genre, genre_id)
+        if genre:
+            self.session.delete(genre)
+            self.session.commit()
+            return True
+        return False
+
+
+    def get_genres_with_most_movies(self) -> list[tuple]:
+        query = (self.session
+            .query(Genre, func.count(MovieGenre.movie_id).label("movie_count"))
+            .join(MovieGenre, Genre.id == MovieGenre.genre_id)
+            .group_by(Genre.id)
+            .order_by(func.count(MovieGenre.movie_id).desc())
+        )
+        return query.all()
+
+
+
+
