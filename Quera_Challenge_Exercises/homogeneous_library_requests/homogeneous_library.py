fe369969513bbf69c5dbef446f89722b7dba87e3
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-08-11 17:12:53 +0200

    intial graphical terminal exercise using pygame library

--- Quera_Challenge_Exercises/homogeneous_library_requests/homogeneous_library.py
+++ Quera_Challenge_Exercises/homogeneous_library_requests/homogeneous_library.py
@@ -1,5 +1,33 @@
 import requests
 
+URL = "http://127.0.0.1:8000/"
 
 def add_book(book: dict[str, str]) -> str or None:
-    pass
+    category = book["category"]
+    if category not in ["mathematics", "physics", "chess" ]:
+        return "Invalid Category"
+
+    url = f"{URL}{category}/"
+
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return "Bad Request"
+    except requests.exceptions.RequestException:
+        return "Bad Request"
+
+
+    data = response.json()
+    for item in data:
+        if item["name"] == book["name"]:
+            return "Bad Request"
+
+    try:
+        post_response = requests.post(url, json=book)
+        if post_response.status_code != 201:
+            return None
+        else:
+            return "Bad Request"
+    except requests.exceptions.RequestException:
+        return "Bad Request"
+
