bb0f189634f7d79dac41d4e1ec3b5af1b29b3a05
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-08-18 14:39:14 +0200

    Completed meaningful words finder exercise

--- Quera_Challenge_Exercises/meaningful_words_finder/meaningful_words_finder.py
+++ Quera_Challenge_Exercises/meaningful_words_finder/meaningful_words_finder.py
@@ -1,31 +1,16 @@
 def check_words(text: str) -> dict[str, int]:
-    raw_words = text.split(" ")
+    raw_words = text.split()
     good_words = []
-    final_words = []
     result = {}
 
     for word in raw_words:
-        alphas_l = []
-        signs_l = []
-        for char in word:
-            if char.isalpha():
-                alphas_l.append(char)
-            else:
-                signs_l.append(char)
+        alphas_l = [char for char in word if char.isalpha()]
+        signs_l = [char for char in word if not char.isalpha() ]
         if len(alphas_l) > len(signs_l):
-            good_words.append(word.capitalize())
-    for word in good_words:
-        w = []
-        for char in word:
-            if char.isalpha():
-                w.append(char)
-        final_words.append("".join(w))
-    for word in final_words:
+            good_words.append("".join(alphas_l).capitalize())
+    for word in sorted(good_words):
         if word not in result:
             result[word] = 1
         else:
             result[word] += 1
     return result
-
-
-print(check_words("""hEllO My FriEnDs!!! thIS is A tEsT For your #p#r#o#b#l#e#m a"""))
